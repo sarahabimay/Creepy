2b976b81da17d73cbd0d0826de47160b2bd2d96c
sarahabimay <sarahabimay@gmail.com>
2015-08-08 23:27:40 +0100

    remove annoying comments

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -32,8 +32,6 @@ module.exports = {
 						return reply.view( "homepage", { alerts: [{isError: true, alert: "Error for URL: " + searchURL }, {isError: true, alert: "Error : " + error }] } );
 					}
 					if( assets ) {
-						console.log( "Assets found: " ) ;
-						console.dir( assets );
 						return reply.view("sitemap", { rootURL: searchURL, urls : assets.urls, css: assets.css, scripts : assets.scripts, images : assets.images });
 					}
 					return reply.view( "homepage", { alerts: [{isError: true, alert: "Error for URL: " + searchURL }, {isError: true, alert: "Error : No Static Assets Found." }] } );
